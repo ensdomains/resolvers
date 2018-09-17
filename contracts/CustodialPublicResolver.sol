8986b880c2f0599f14f333c38b236650d7700de6
decanus <dean.eigenmann@icloud.com>
2018-09-17 11:29:12 +0200

    rough draft

---
+++ contracts/CustodialPublicResolver.sol
@@ -0,0 +1,102 @@
+pragma solidity ^0.4.24;
+
+import "@ensdomains/ens/contracts/PublicResolver.sol";
+import "@dexyproject/signature-validator/contracts/SignatureValidator.sol";
+
+// @todo think of a better name
+contract CustodialPublicResolver is PublicResolver {
+
+    bytes32 constant public SET_ADDR_HASH_SCHEME = keccak256(
+        "bytes32 Node",
+        "address Address",
+        "uint256 Nonce"
+    );
+
+    bytes32 constant public SET_CONTENT_HASH_SCHEME = keccak256(
+        "bytes32 Node",
+        "bytes32 Hash",
+        "uint256 Nonce"
+    );
+
+    bytes32 constant public SET_MULTIHASH_HASH_SCHEME = keccak256(
+        "bytes32 Node",
+        "bytes Hash",
+        "uint256 Nonce"
+    );
+
+    bytes32 constant public SET_NAME_HASH_SCHEME = keccak256(
+        "bytes32 Node",
+        "string Name",
+        "uint256 Nonce"
+    );
+
+    bytes32 constant public SET_ABI_HASH_SCHEME = keccak256(
+        "bytes32 Node",
+        "uint256 Content Type",
+        "bytes Data",
+        "uint256 Nonce"
+    );
+
+    bytes32 constant public SET_PUBKEY_HASH_SCHEME = keccak256(
+        "bytes32 Node",
+        "bytes32 X",
+        "bytes32 Y",
+        "uint256 Nonce"
+    );
+
+    bytes32 constant public SET_TEXT_HASH_SCHEME = keccak256(
+        "bytes32 Node",
+        "string Key",
+        "string Value",
+        "uint256 Nonce"
+    );
+
+    mapping (bytes32 => bool) public submitted;
+
+    function setAddrOnBehalfOf(bytes32 node, address addr, uint256 nonce, address signer, bytes signature) public {
+        validateSignature(keccak256(SET_ADDR_HASH_SCHEME, keccak256(node, addr, nonce)), signer, signature);
+    }
+
+    function setContentOnBehalfOf(bytes32 node, bytes32 hash, uint256 nonce, address signer, bytes signature) public {
+
+    }
+
+    function setMultihashOnBehalfOf(bytes32 node, bytes hash, uint256 nonce, address signer, bytes signature) public {
+
+    }
+
+    function setNameOnBehalfOf(bytes32 node, string name, uint256 nonce, address signer, bytes signature) public {
+
+    }
+
+    function setABIOnBehalfOf(
+        bytes32 node,
+        uint256 contentType,
+        bytes data,
+        uint256 nonce,
+        address signer,
+        bytes signature
+    )
+        public
+    {
+
+    }
+
+    function setPubkeyOnBehalfOf(bytes32 node, bytes32 x, bytes32 y, uint256 nonce, address signer, bytes signature)
+        public
+    {
+
+    }
+
+    function setTextOnBehalfOf(bytes32 node, string key, string value, uint256 nonce, address signer, bytes signature)
+        public
+    {
+
+    }
+
+    function validateSignature(bytes32 hash, address signer, bytes signature) private {
+        require(!submitted[hash]);
+        require(SignatureValidator.isValidSignature(hash, signer, signature));
+        submitted[hash] = true;
+    }
+}
