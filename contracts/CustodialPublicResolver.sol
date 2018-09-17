c5568ae29ea507289e50323dd69aaaafdbc86e84
decanus <dean.eigenmann@icloud.com>
2018-09-17 12:01:08 +0200

    removed erc712 related stuff, implemented all functions

--- contracts/CustodialPublicResolver.sol
+++ contracts/CustodialPublicResolver.sol
@@ -6,81 +6,51 @@ import "@dexyproject/signature-validator/contracts/SignatureValidator.sol";
 // @todo think of a better name
 contract CustodialPublicResolver is PublicResolver {
 
-    bytes32 constant public SET_ADDR_HASH_SCHEME = keccak256(
-        "bytes32 Node",
-        "address Address",
-        "uint256 Nonce"
-    );
-
-    bytes32 constant public SET_CONTENT_HASH_SCHEME = keccak256(
-        "bytes32 Node",
-        "bytes32 Hash",
-        "uint256 Nonce"
-    );
-
-    bytes32 constant public SET_MULTIHASH_HASH_SCHEME = keccak256(
-        "bytes32 Node",
-        "bytes Hash",
-        "uint256 Nonce"
-    );
-
-    bytes32 constant public SET_NAME_HASH_SCHEME = keccak256(
-        "bytes32 Node",
-        "string Name",
-        "uint256 Nonce"
-    );
-
-    bytes32 constant public SET_ABI_HASH_SCHEME = keccak256(
-        "bytes32 Node",
-        "uint256 Content Type",
-        "bytes Data",
-        "uint256 Nonce"
-    );
-
-    bytes32 constant public SET_PUBKEY_HASH_SCHEME = keccak256(
-        "bytes32 Node",
-        "bytes32 X",
-        "bytes32 Y",
-        "uint256 Nonce"
-    );
-
-    bytes32 constant public SET_TEXT_HASH_SCHEME = keccak256(
-        "bytes32 Node",
-        "string Key",
-        "string Value",
-        "uint256 Nonce"
-    );
-
     mapping (bytes32 => bool) public submitted;
 
     function setAddrOnBehalfOf(bytes32 node, address addr, uint256 nonce, bytes signature) public {
-        validateSignature(node, keccak256(SET_ADDR_HASH_SCHEME, keccak256(node, addr, nonce)), signature);
+        validateSignature(node, keccak256(node, addr, nonce), signature);
         records[node].addr = addr;
         AddrChanged(node, addr);
     }
 
     function setContentOnBehalfOf(bytes32 node, bytes32 hash, uint256 nonce, bytes signature) public {
-
+        validateSignature(node, keccak256(node, hash, nonce), signature);
+        records[node].content = hash;
+        ContentChanged(node, hash);
     }
 
     function setMultihashOnBehalfOf(bytes32 node, bytes hash, uint256 nonce, bytes signature) public {
-
+        validateSignature(node, keccak256(node, hash, nonce), signature);
+        records[node].multihash = hash;
+        MultihashChanged(node, hash);
     }
 
     function setNameOnBehalfOf(bytes32 node, string name, uint256 nonce, bytes signature) public {
-
+        validateSignature(node, keccak256(node, name, nonce), signature);
+        records[node].name = name;
+        NameChanged(node, name);
     }
 
     function setABIOnBehalfOf(bytes32 node, uint256 contentType, bytes data, uint256 nonce, bytes signature) public {
+        validateSignature(node, keccak256(node, contentType, data, nonce), signature);
 
+        require(((contentType - 1) & contentType) == 0);
+
+        records[node].abis[contentType] = data;
+        ABIChanged(node, contentType);
     }
 
     function setPubkeyOnBehalfOf(bytes32 node, bytes32 x, bytes32 y, uint256 nonce, bytes signature) public {
-
+        validateSignature(node, keccak256(node, x, y, nonce), signature);
+        records[node].pubkey = PublicKey(x, y);
+        PubkeyChanged(node, x, y);
     }
 
     function setTextOnBehalfOf(bytes32 node, string key, string value, uint256 nonce, bytes signature) public {
-
+        validateSignature(node, keccak256(node, key, value, nonce), signature);
+        records[node].text[key] = value;
+        TextChanged(node, key, key);
     }
 
     function validateSignature(bytes32 node, bytes32 hash, bytes signature) private {
