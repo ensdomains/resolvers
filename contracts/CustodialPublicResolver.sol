f459e2bbada4c040c26f02325c5eb951db7bed6b
decanus <dean.eigenmann@icloud.com>
2018-09-17 11:40:19 +0200

    removed signer

--- contracts/CustodialPublicResolver.sol
+++ contracts/CustodialPublicResolver.sol
@@ -53,52 +53,40 @@ contract CustodialPublicResolver is PublicResolver {
 
     mapping (bytes32 => bool) public submitted;
 
-    function setAddrOnBehalfOf(bytes32 node, address addr, uint256 nonce, address signer, bytes signature) public {
-        validateSignature(keccak256(SET_ADDR_HASH_SCHEME, keccak256(node, addr, nonce)), signer, signature);
+    function setAddrOnBehalfOf(bytes32 node, address addr, uint256 nonce, bytes signature) public {
+        validateSignature(node, keccak256(SET_ADDR_HASH_SCHEME, keccak256(node, addr, nonce)), signature);
         records[node].addr = addr;
         AddrChanged(node, addr);
     }
 
-    function setContentOnBehalfOf(bytes32 node, bytes32 hash, uint256 nonce, address signer, bytes signature) public {
+    function setContentOnBehalfOf(bytes32 node, bytes32 hash, uint256 nonce, bytes signature) public {
 
     }
 
-    function setMultihashOnBehalfOf(bytes32 node, bytes hash, uint256 nonce, address signer, bytes signature) public {
+    function setMultihashOnBehalfOf(bytes32 node, bytes hash, uint256 nonce, bytes signature) public {
 
     }
 
-    function setNameOnBehalfOf(bytes32 node, string name, uint256 nonce, address signer, bytes signature) public {
+    function setNameOnBehalfOf(bytes32 node, string name, uint256 nonce, bytes signature) public {
 
     }
 
-    function setABIOnBehalfOf(
-        bytes32 node,
-        uint256 contentType,
-        bytes data,
-        uint256 nonce,
-        address signer,
-        bytes signature
-    )
-        public
-    {
+    function setABIOnBehalfOf(bytes32 node, uint256 contentType, bytes data, uint256 nonce, bytes signature) public {
 
     }
 
-    function setPubkeyOnBehalfOf(bytes32 node, bytes32 x, bytes32 y, uint256 nonce, address signer, bytes signature)
-        public
-    {
+    function setPubkeyOnBehalfOf(bytes32 node, bytes32 x, bytes32 y, uint256 nonce, bytes signature) public {
 
     }
 
-    function setTextOnBehalfOf(bytes32 node, string key, string value, uint256 nonce, address signer, bytes signature)
-        public
-    {
+    function setTextOnBehalfOf(bytes32 node, string key, string value, uint256 nonce, bytes signature) public {
 
     }
 
-    function validateSignature(bytes32 hash, address signer, bytes signature) private {
+    function validateSignature(bytes32 node, bytes32 hash, bytes signature) private {
         require(!submitted[hash]);
-        require(SignatureValidator.isValidSignature(hash, signer, signature));
+        address signer = SignatureValidator.isValidSignature(hash, signer, signature);
+        require(ens.owner(node) == signer);
         submitted[hash] = true;
     }
 }
