0a56756f54cf29a571fe2ea0d55a3c0c89a9bdbb
decanus <dean.eigenmann@icloud.com>
2018-09-17 12:05:59 +0200

    renamed

--- contracts/CustodialPublicResolver.sol
+++ contracts/CustodialPublicResolver.sol
@@ -8,31 +8,31 @@ contract CustodialPublicResolver is PublicResolver {
 
     mapping (bytes32 => bool) public submitted;
 
-    function setAddrOnBehalfOf(bytes32 node, address addr, uint256 nonce, bytes signature) public {
+    function setAddrFor(bytes32 node, address addr, uint256 nonce, bytes signature) public {
         validateSignature(node, keccak256(node, addr, nonce), signature);
         records[node].addr = addr;
         emit AddrChanged(node, addr);
     }
 
-    function setContentOnBehalfOf(bytes32 node, bytes32 hash, uint256 nonce, bytes signature) public {
+    function setContentFor(bytes32 node, bytes32 hash, uint256 nonce, bytes signature) public {
         validateSignature(node, keccak256(node, hash, nonce), signature);
         records[node].content = hash;
         emit ContentChanged(node, hash);
     }
 
-    function setMultihashOnBehalfOf(bytes32 node, bytes hash, uint256 nonce, bytes signature) public {
+    function setMultihashFor(bytes32 node, bytes hash, uint256 nonce, bytes signature) public {
         validateSignature(node, keccak256(node, hash, nonce), signature);
         records[node].multihash = hash;
         emit MultihashChanged(node, hash);
     }
 
-    function setNameOnBehalfOf(bytes32 node, string name, uint256 nonce, bytes signature) public {
+    function setNameFor(bytes32 node, string name, uint256 nonce, bytes signature) public {
         validateSignature(node, keccak256(node, name, nonce), signature);
         records[node].name = name;
         emit NameChanged(node, name);
     }
 
-    function setABIOnBehalfOf(bytes32 node, uint256 contentType, bytes data, uint256 nonce, bytes signature) public {
+    function setABIFor(bytes32 node, uint256 contentType, bytes data, uint256 nonce, bytes signature) public {
         validateSignature(node, keccak256(node, contentType, data, nonce), signature);
 
         require(((contentType - 1) & contentType) == 0);
@@ -41,13 +41,13 @@ contract CustodialPublicResolver is PublicResolver {
         emit ABIChanged(node, contentType);
     }
 
-    function setPubkeyOnBehalfOf(bytes32 node, bytes32 x, bytes32 y, uint256 nonce, bytes signature) public {
+    function setPubkeyFor(bytes32 node, bytes32 x, bytes32 y, uint256 nonce, bytes signature) public {
         validateSignature(node, keccak256(node, x, y, nonce), signature);
         records[node].pubkey = PublicKey(x, y);
         emit PubkeyChanged(node, x, y);
     }
 
-    function setTextOnBehalfOf(bytes32 node, string key, string value, uint256 nonce, bytes signature) public {
+    function setTextFor(bytes32 node, string key, string value, uint256 nonce, bytes signature) public {
         validateSignature(node, keccak256(node, key, value, nonce), signature);
         records[node].text[key] = value;
         emit TextChanged(node, key, key);
