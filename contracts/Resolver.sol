ae145cb2de619ac428bf254f6b139f0adefd8b02
Nick Johnson <arachnid@notdot.net>
2019-04-23 16:42:57 +1200

    Reformat resolver interface, add interface functions

--- contracts/Resolver.sol
+++ contracts/Resolver.sol
@@ -19,22 +19,19 @@ interface Resolver{
     function dnsrr(bytes32 node) external view returns (bytes memory);
     function name(bytes32 node) external view returns (string memory);
     function pubkey(bytes32 node) external view returns (bytes32 x, bytes32 y);
+    function text(bytes32 node, string calldata key) external view returns (string memory);
+    function interfaceImplementer(bytes32 node, bytes4 interfaceID) external view returns (address);
+
     function setABI(bytes32 node, uint256 contentType, bytes calldata data) external;
-    function setABIFor(bytes32 node, uint256 contentType, bytes calldata data, bytes calldata signature) external;
     function setAddr(bytes32 node, address addr) external;
-    function setAddrFor(bytes32 node, address addr, bytes calldata signature) external;
-    function setAuthorisation(bytes32 node, address target, bool isAuthorised) external;
     function setContenthash(bytes32 node, bytes calldata hash) external;
-    function setContenthashFor(bytes32 node, bytes calldata hash, bytes calldata signature) external;
     function setDnsrr(bytes32 node, bytes calldata data) external;
     function setName(bytes32 node, string calldata _name) external;
-    function setNameFor(bytes32 node, string calldata name, bytes calldata signature) external;
     function setPubkey(bytes32 node, bytes32 x, bytes32 y) external;
-    function setPubkeyFor(bytes32 node, bytes32 x, bytes32 y, bytes calldata signature) external;
     function setText(bytes32 node, string calldata key, string calldata value) external;
-    function setTextFor(bytes32 node, string calldata key, string calldata value, bytes calldata signature) external;
+    function setInterface(bytes32 node, bytes4 interfaceID, address implementer) external;
+
     function supportsInterface(bytes4 interfaceID) external pure returns (bool);
-    function text(bytes32 node, string calldata key) external view returns (string memory);
 
     /* Deprecated functions */
     function content(bytes32 node) external view returns (bytes32);
