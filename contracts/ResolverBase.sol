5de07f1f4d274c9354c3c98ea82a1e2e67e9ceb7
Nick Johnson <arachnid@notdot.net>
2019-09-25 13:05:22 +1200

    Restructure multicoin support to use maps

--- contracts/ResolverBase.sol
+++ contracts/ResolverBase.sol
@@ -17,14 +17,14 @@ contract ResolverBase {
     function bytesToAddress(bytes memory b) internal pure returns(address a) {
         require(b.length == 20);
         assembly {
-            a := mload(add(b, 32))
+            a := div(mload(add(b, 32)), exp(256, 12))
         }
     }
 
     function addressToBytes(address a) internal pure returns(bytes memory b) {
         b = new bytes(20);
         assembly {
-            mstore(add(b, 32), a)
+            mstore(add(b, 32), mul(a, exp(256, 12)))
         }
     }
 }
--- contracts/profiles/AddrResolver.sol
+++ contracts/profiles/AddrResolver.sol
@@ -1,22 +1,16 @@
 pragma solidity ^0.5.8;
-pragma experimental ABIEncoderV2;
 
 import "../ResolverBase.sol";
 
 contract AddrResolver is ResolverBase {
     bytes4 constant private ADDR_INTERFACE_ID = 0x3b3b57de;
-    bytes4 constant private ADDRESS_INTERFACE_ID = 0x0;
+    bytes4 constant private ADDRESS_INTERFACE_ID = 0xf1cb7e06;
     uint constant private COIN_TYPE_ETH = 60;
 
     event AddrChanged(bytes32 indexed node, address a);
     event AddressChanged(bytes32 indexed node, uint coinType, bytes newAddress);
 
-    struct AddressInfo {
-        uint coinType;
-        bytes addr;
-    }
-
-    mapping(bytes32=>AddressInfo[]) _addresses;
+    mapping(bytes32=>mapping(uint=>bytes)) _addresses;
 
     /**
      * Sets the address associated with an ENS node.
@@ -25,11 +19,7 @@ contract AddrResolver is ResolverBase {
      * @param a The address to set.
      */
     function setAddr(bytes32 node, address a) external authorised(node) {
-        if(a == address(0)) {
-            deleteAddress(node, COIN_TYPE_ETH);
-        } else {
-            setAddress(node, COIN_TYPE_ETH, addressToBytes(a));
-        }
+        setAddr(node, COIN_TYPE_ETH, addressToBytes(a));
     }
 
     /**
@@ -45,47 +35,16 @@ contract AddrResolver is ResolverBase {
         return bytesToAddress(a);
     }
 
-    function setAddress(bytes32 node, uint coinType, bytes memory a) public authorised(node) {
-        AddressInfo[] storage addrs = _addresses[node];
+    function setAddr(bytes32 node, uint coinType, bytes memory a) public authorised(node) {
         emit AddressChanged(node, coinType, a);
         if(coinType == COIN_TYPE_ETH) {
             emit AddrChanged(node, bytesToAddress(a));
         }
-        for(uint i = 0; i < addrs.length; i++) {
-            if(addrs[i].coinType == coinType) {
-                addrs[i].addr = a;
-                return;
-            }
-        }
-        addrs.push(AddressInfo(coinType, a));
-    }
-
-    function deleteAddress(bytes32 node, uint coinType) public authorised(node) {
-        AddressInfo[] storage addrs = _addresses[node];
-        for(uint i = 0; i < addrs.length; i++) {
-            if(addrs[i].coinType == coinType) {
-                if(i < addrs.length - 1) {
-                    addrs[i] = addrs[addrs.length - 1];
-                }
-                addrs.length--;
-                emit AddressChanged(node, coinType, "");
-                return;
-            }
-        }
-    }
-
-    function addresses(bytes32 node) external view returns(AddressInfo[] memory) {
-        return _addresses[node];
+        _addresses[node][coinType] = a;
     }
 
     function addr(bytes32 node, uint coinType) public view returns(bytes memory) {
-        AddressInfo[] storage addrs = _addresses[node];
-        for(uint i = 0; i < addrs.length; i++) {
-            if(addrs[i].coinType == coinType) {
-                return addrs[i].addr;
-            }
-        }
-        return "";
+        return _addresses[node][coinType];
     }
 
     function supportsInterface(bytes4 interfaceID) public pure returns(bool) {
