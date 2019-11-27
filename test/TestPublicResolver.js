e5224d9b757149f7823c1c156a67c37c7172587b
Nick Johnson <arachnid@notdot.net>
2019-11-27 20:20:05 +1300

    Add multicall support, and fix leftover reference to the custodial resolver

--- test/TestPublicResolver.js
+++ test/TestPublicResolver.js
@@ -659,6 +659,40 @@ contract('PublicResolver', function (accounts) {
             assert.equal(await resolver.addr(node), accounts[0]);
         });
     });
+
+    describe('multicall', async () => {
+        it('allows setting multiple fields', async () => {
+          var addrSet = resolver.contract.methods['setAddr(bytes32,address)'](node, accounts[1]).encodeABI();
+          var textSet = resolver.contract.methods.setText(node, "url", "https://ethereum.org/").encodeABI();
+          var tx = await resolver.multicall([addrSet, textSet], {from: accounts[0]});
+
+          assert.equal(tx.logs.length, 3);
+          assert.equal(tx.logs[0].event, "AddressChanged");
+          assert.equal(tx.logs[0].args.node, node);
+          assert.equal(tx.logs[0].args.newAddress, accounts[1].toLowerCase());
+          assert.equal(tx.logs[1].event, "AddrChanged");
+          assert.equal(tx.logs[1].args.node, node);
+          assert.equal(tx.logs[1].args.a, accounts[1]);
+          assert.equal(tx.logs[2].event, "TextChanged");
+          assert.equal(tx.logs[2].args.node, node);
+          assert.equal(tx.logs[2].args.key, "url");
+
+          assert.equal(await resolver.methods['addr(bytes32)'](node), accounts[1]);
+          assert.equal(await resolver.text(node, "url"), "https://ethereum.org/");
+        });
+
+        // Disabled because web3 doesn't have a function for decoding the return ABI
+        // it('allows reading multiple fields', async () => {
+        //   await resolver.methods['setAddr(bytes32,address)'](node, accounts[1], {from: accounts[0]});
+        //   await resolver.setText(node, "url", "https://ethereum.org/", {from: accounts[0]});
+        //   var results = await resolver.multicall.call([
+        //     resolver.contract.methods['addr(bytes32)'](node).encodeABI(),
+        //     resolver.contract.methods.text(node, "url").encodeABI()
+        //   ]);
+        //   assert.equal(results[0], accounts[1]);
+        //   assert.equal(results[1], "https://ethereum.org/");
+        // });
+    });
 });
 
 function dnsName(name) {
@@ -680,4 +714,3 @@ function dnsName(name) {
     buf[offset++] = 0;
     return '0x' + buf.reduce((output, elem) => (output + ('0' + elem.toString(16)).slice(-2)), '');
 }
-
